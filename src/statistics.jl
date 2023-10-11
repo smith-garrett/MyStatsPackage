b2daaf506dd5bfdb9c82107df0f11fcd579be12e
Garrett Smith <gasmith@uni-potsdam.de>
2023-10-11 09:41:01 +0200

    Add docstrings, remove progress bar.

--- src/statistics.jl
+++ src/statistics.jl
@@ -1,8 +1,11 @@
-using ProgressMeter
+"""
+    rse_sum(x)
 
+Return the sum of the elements in x.
+"""
 function rse_sum(x)
     sm = zero(eltype(x))
-    @showprogress dt = 0.1 for i in eachindex(x)
+    for i in eachindex(x)
         sm += x[i]
     end
     return sm
@@ -10,6 +13,11 @@ end
 
 rse_sum(1:36) == 666
 
+"""
+    rse_mean(x)
+
+Return the mean of the elements in x.
+"""
 function rse_mean(x)
     sm = rse_sum(x)
     return sm / length(x)
@@ -17,6 +25,11 @@ end
 
 rse_mean(-15:17) == 1
 
+"""
+    rse_std(x)
+
+Return the standard deviation of the elements in x.
+"""
 function rse_std(x)
     ln = length(x)
     return sqrt(sum((x .- rse_mean(x)) .^ 2) / (ln - 1))
@@ -24,7 +37,13 @@ end
 
 rse_std(1:3) == 1
 
-#function rse_tstat(x; σ=nothing)
+"""
+    rse_tstat(x; σ = rse_std(x))
+
+Return the t-statistic for the comparison of the elements of x to an assumed mean of 0. The 
+optional σ parameter can be used to provide a known standard deviation, otherwise it is 
+calculated from the data.
+"""
 function rse_tstat(x; σ = rse_std(x))
     mn = rse_mean(x)
     #=
@@ -36,12 +55,11 @@ function rse_tstat(x; σ = rse_std(x))
     return mn / (σ / sqrt(ln))
 end
 
-rse_tstat(2:3) == 5
+"""
+    StatResult(x, n, std, tvalue)
 
-rse_tstat(rand(10_000))
-
-# Task 2:
-
+A type for storing t-statistics from data.
+"""
 struct StatResult
     x::Vector
     n::Int64
@@ -49,5 +67,9 @@ struct StatResult
     tvalue::Float64
 end
 
+"""
+    StatResult(data)
+
+Create an instance of StatResult, calculating the statistics from the argument data.
+"""
 StatResult(data) = StatResult(collect(data), length(data), rse_std(data), rse_tstat(data))
-r = StatResult(2:10)
